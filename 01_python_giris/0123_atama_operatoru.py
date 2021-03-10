20358594733c63a55c27fc616b5a9f26610a2796
batukochan <batukochan@icloud.com>
2021-03-10 19:59:29 +0300

    dersler5

--- 01_python_giris/0123_atama_operatoru.py
+++ 01_python_giris/0123_atama_operatoru.py
@@ -1,6 +1,8 @@
 #region değişken kavramı
-
+"""
 kg=76
 boy=1.65
 vki=kg/(boy**2)
 print("Girdiniğiniz bilgilere göre vücut kitle indeksiniz : ", round(vki,2))
+"""
+
---
+++ 01_python_giris/0126_casting.py
@@ -0,0 +1,12 @@
+"""kullanıcının doğum tarihini alalım"""
+"""
+print("***Yaş Hesaplama Programına Hoşgeldiniz***")
+dTarih=int(input("Doğum tarihinizi giriniz (yıl)\t: " ))
+yas=2021-dTarih
+print("Doğum yılı",dTarih,"olan bireyler ",yas,"yaşındadır.")
+"""
+#a kenareı,b kenarı girilecek dörtgen için alan hesaplanacak
+a=int(input("Lütfen kısa kenar bilgisi veriniz : "))
+b=int(input("Lütfen uzun bilgisi veriniz : "))
+islem=a*b
+print("Kısa kenarı",a,"m olan ve uzun kenarı",b,"m olan dikdörtgenin alanı :",islem," m^2")

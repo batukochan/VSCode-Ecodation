4cb175792632d86467ae1c9258bc9bfd400ab903
batukochan <batukochan@icloud.com>
2021-03-15 20:22:46 +0300

    dersler1

--- 02_karar_yapilari/0205_break_point.py
+++ 02_karar_yapilari/0205_break_point.py
@@ -1,7 +1,9 @@
-#break-point
+#region break-point
+"""
 kAdi = input('Kullanici Adi Giriniz: ')
 print("a")
 if kAdi!= "admin":
     print(f"{kAdi} yetkisi ile admin paneline giremezsiniz...")
     print("b")
 print("c")
+"""

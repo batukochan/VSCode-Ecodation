8c8005998ed05b989b09ba9086594c25391b59ae
batukochan <batukochan@icloud.com>
2021-04-11 18:09:16 +0300

    çalışma

--- 04_liste_string_metodlar.py/0409_string.py
+++ 04_liste_string_metodlar.py/0409_string.py
@@ -1,7 +1,22 @@
+#region giriş
 """
 ad = "batuhan"
-print(ad[0])
+print(ad[0]) #0. indexte yer alan b harfini bastırır.
 """
+#endregion 
+
+# region string_iterable_giris
+"""
+ad = "BATUHAN"
+print(ad[2]) #T
+meyveler = ["elma", "armut", "muz", "ayva", "üzüm", "elma"]
+print(meyveler[3]) #ayva
+kurum = "Flux"
+print(kurum[3]) #x
+"""
+# endregion
+
+#region sesli sessiz harf
 """ 
 meyveler = ["elma","armut","muz","ayva","üzüm","elma"]
 print(meyveler[1])
@@ -9,8 +24,8 @@ print(meyveler[1])
 """
 ad = "batuhan"
 sesliHarf = ['a','e','o','u','ö','ü','ı','i']
-ad = "batuhan"
 for i in range(0,len(ad)):
     if ad[i] not in sesliHarf:
         print(ad[i],end="")
 """
+#endregion

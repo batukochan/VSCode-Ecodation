f4fa0aff9a1fccf6f5f1943266d0a51953d52573
batukochan <batukochan@icloud.com>
2021-04-12 01:24:46 +0300

    çalışma

--- 07_ornekler/örnek 3/sezar_sifreleme_teknigi.py
+++ 07_ornekler/örnek 3/sezar_sifreleme_teknigi.py
@@ -1,9 +1,23 @@
 #region sezar şifre
-metin = "Python"
+"""
+metin = "Merhaba Dünya""
 parola = ""
 for i in metin:
     print(ord(i)) #ASCII kodlarını döndürür.,
     print(i," => ",chr(ord(i)+5))
     parola = parola + chr(ord(i)+5)
 print(metin,parola)
+"""
+#endregion
+
+#region sezar şifreyi çözme
+"""
+metin = ""
+parola ="Rjwmfgf%Iās~f" 
+for i in parola:
+    print(ord(i)) #ASCII kodlarını döndürür.,
+    print(i," => ",chr(ord(i)-5))
+    metin = metin + chr(ord(i)-5)
+print(parola,metin)
+"""
 #endregion

1068f79f66ca1bea5950a1718c2b7aa789ad6359
batukochan <batukochan@icloud.com>
2021-04-12 19:52:07 +0300

    çalışma

---
+++ 04_liste_string_metodlar.py/0414_string_metodlari.py
@@ -0,0 +1,11 @@
+#region string metodları
+'''
+upper() lower() capatilize()
+count() replace() swapcase()
+startswith() endswith()
+strip() lstrip() rstrip()
+isdigit() isalpha()
+isalphaum() issapace() istitle() isidentifier()
+split() index() format()
+'''
+#endregion

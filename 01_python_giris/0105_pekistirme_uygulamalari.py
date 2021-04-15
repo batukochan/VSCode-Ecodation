648fc84676eb09cbdb4e356afa3a5634e1d9e308
batukochan <batukochan@icloud.com>
2021-04-15 20:24:27 +0300

    çalışma

--- 01_python_giris/0105_pekistirme_uygulamalari.py
+++ 01_python_giris/0105_pekistirme_uygulamalari.py
@@ -1,19 +1,20 @@
-  
 #region ornek_1
 """
-Dünya'nın♥En♥Güzel♥Şehri→İstanbul.
+Dünya'nın ♥ En ♥ Güzel ♥ Şehri → İstanbul'dur.
 alt+3  ♥
 alt + 26 →
-print("Dünya'nın", "En", "Güzel", "Şehri", sep="♥", end="→")
-print("İstanbul", end=".")
+
+print("Dünya'nın", "En", "Güzel", "Şehri", sep=" ♥ ", end=" → ")
+print("İstanbul'dur", end=".")
 """
 #endregion
 
 
 #region ornek_2
 """
-"Ege'nin"☼"İncisi"☼"İzmir"!
-alt+15  ☼
+"Ege'nin"☼"İncisi"☼"İzmir!"
+alt + 15 = ☼
 """
 print("\"Ege'nin\"", "\"İncisi\"", "\"İzmir!\"", sep="☼")
+
 #endregion

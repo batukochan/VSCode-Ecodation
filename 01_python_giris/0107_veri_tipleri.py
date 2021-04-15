122e12df4b52b7f2c97d671a86db86c8867abbf6
batukochan <batukochan@icloud.com>
2021-04-15 22:02:57 +0300

    çalışma

--- 01_python_giris/0107_veri_tipleri.py
+++ 01_python_giris/0107_veri_tipleri.py
@@ -1,36 +1,42 @@
 #region int
 #ilk veri tipimiz integer
-
+"""
 print(1400) #örneğin bir mesafe olabilir
 print(86) #bir sınav notu olabilir
 print(36) #normal vucüt ısısını temsil edebilir.
 print(10000)
+"""
 #endregion
 
 #region float
 
 # ikinci veri tipi float
+"""
 print(3.14)
 print(1.82)
 print(348.34)
+"""
 #endregion
 
 #region string
-
-print("ecodation")
+"""
+print("Flux")
 print("python")
+"""
 #endregion
 
 #region boolean
+"""
 print(True)
 print(False)
+"""
 #endregion
 
-#region TYPE
-#parantez içindeki değerin tipini geri döndürür.
-
+#region 
+# TYPE → Parantez içindeki değerin tipini geri döndürür.
 print(type(2021))
-print(type("alfa"))
-print(type(23.4))
+print(type("Veri Tipleri"))
 print(type(True))
+print(type(3.14))
+print(type(3.14+52j))
 #endregion
--- 01_python_giris/0108_operatorlar.py
+++ 01_python_giris/0108_operatorlar.py
@@ -22,51 +22,80 @@ print(.15 - 15)
 #endregion
 
 #region +/- unary işaret
-"""
+""" 
 print(+2)
 print(-2)
 """
 #endregion
 
 #region */ 
-"""
+'''
 print(4*4)
+# → 16
 print(.4*4)
+# → 1.6
 print(type(.4*4))
+# → <class 'float'>
 print(9/3)
+# →3.0
 print(9/2)
+# → 4.5
 print(type(9/2))
+# → <class 'float'>
 print(17/.4)
-print(10/0)
+# → 42.5
+'''
 """
+print(10/0)
+# → ZeroDivisionError: division by zero
+"""
+
 #endregion
 
 #region ** üst alma
 """
-print(4**4)
 print(2**4)
-print(16**0.5)
-print(16**(1/2))
+# → 16
+print(3**3)
+# → 27
+print(16**0.5) #köklü ifade
+# → 4.0
+print(16**(1/2)) #köklü ifade
+# → 4.0
 print(type(16**0.5))
+# → <class 'float'>
 """
 #endregion
 
 #region // - tam bölme
 """
 print(12/7)
+# → 1.7142857142857142
 print(12//7)
+# → 1
 print(12//7.)
+# → 1.0
 print(-13/5)
-print(-13//5) #*****
+# → -2.6
+print(-12//5) # Önemli -2.4
+# → -3
+print(-13//5) # Önemli -2.6
+# → -3
 """
 #endregion
 
 #region % - mod alma - kalanı bulma
 """
 print(15%4)
+# 15'in 4'e bölümünden kalan  → 3
 print(15%2) #***
+# 15'in 2'ye bölümünden kalan → 1
 print(8%3)
+# 8'in 3'e bölümünden kalan   → 1
+"""
+"""
 print(15%0)
+#ZeroDivisionError: integer division or modulo by zero
 """
 #endregion
 
@@ -77,10 +106,20 @@ print(15%0)
 3→  *, /, %     çarpma, bölme, mod alma
 4→  +,-         toplama çıkarma
 """
-"""
+""""""
+
+#region
+
 print(3+4*5)
-print(15%4*2)   #% left-side binding
-print(15%4%2)   #% left-side binding
-print(2**2**3)  #** right-side binding
-"""
+# 1.İşlem → 4 * 5 = 20 | 2.İşlem → 3 + 20 = 23 || sonuç → 23
+
+print(15%4*2)   # % left-side 
+# 1.İşlem → 15 % 4 = 3 | 2.İşlem → 3 * 2 = 6   || sonuç → 6
+
+print(15%4%2)   # % left-side binding
+# 1.İşlem → 15 % 4 = 3 | 2.İşlem → 3 % 2 = 1   || sonuç → 1
+
+print(2**2**3)  # ** right-side binding
+# 1.İşlem → 2**3 = 8   | 2.İşlem → 2**8 = 256  | sonuç → 256
+
 #endregion

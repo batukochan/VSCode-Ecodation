ad8ab4057333c9e35e2bb4615dd4e63c70a807af
batukochan <batukochan@icloud.com>
2021-04-19 21:44:51 +0300

    çalışma

--- 05_fonskiyonlar/0512_none.py
+++ 05_fonskiyonlar/0512_none.py
@@ -31,10 +31,64 @@ def sonuc():
 print(sonuc())
 """
 
+"""
 def birlestir(a,b):
     return f"{a.title()} {b.title()}"
 
 ad= input("İsim: ")
 soyad= input("Soyisim: ")
 print(birlestir(ad,soyad))
+"""
 
+"""
+yazıTura()
+ yazıTura()
+. random değere göre → 'yazı' - 'tura' return edeceğiz
+. while True içinde sonsuz döngü için de yazı için 'y', tura için 't', çıkmak için 'ç'
+. 5 kez kazanan oyunu bitirir.
+. her döngü de skor ekrana yazılacak
+
+"""
+
+"""
+import random
+
+def yaziTura():
+    if random.randint(1,2) == 1 :
+        return 'yazı'
+    else:
+        return 'tura'
+
+def skorYaz():
+    print(f'''
+    Skorunuz : {skor}
+    Pc Skoru : {pcSkor}
+    '''
+    )
+
+skor, pcSkor=3,0
+while True:
+    skorYaz()
+    if skor == 5 or pcSkor ==5:
+        print("Oyun bitti")
+        if skor>pcSkor:
+            print("Kazandınız")
+        else:
+            print("BEN KAZANDIM")
+        break
+    tahmin= input("yazı için 'y', tura için 't', çıkmak için 'ç'")
+    if tahmin.lower() == 'ç' :
+        break
+    if tahmin.lower() == 'y' :
+        if yaziTura() is 'yazı':
+            skor += 1
+        else:
+            pcSkor += 1
+    elif tahmin.lower() == 't' :
+        if yaziTura() is 'tura':
+            skor += 1
+        else:
+            pcSkor += 1
+    else:
+        print("Lütfen 'yazı','tura' ya da 'ç' giriniz...")
+"""

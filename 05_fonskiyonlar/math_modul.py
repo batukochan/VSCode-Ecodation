1044f474c8219904d3c62ed805dac3d00893dc09
batukochan <batukochan@icloud.com>
2021-04-21 15:39:40 +0300

    çalışma

---
+++ 05_fonskiyonlar/math_modul.py
@@ -0,0 +1,36 @@
+import math
+"""
+print(math)
+"""
+# <module 'math' (built-in)>
+"""
+print(math.sqrt(16))
+"""
+# karakök işlemidir. sonuç = 4.0
+
+# region örnek 1
+'''Yarıçapı 4 olan çemberin çevresini hesaplayalım'''
+
+"""
+r = 4
+cevre = 2*math.pi*r
+print(round(cevre, 2))
+"""
+# endregion
+
+# region örnek 2
+"""
+derece = 30
+radyan = math.radians(derece)
+print(round(radyan, 3))
+"""
+# endregion
+
+# region örnek 3
+"""
+derece = 30
+radyan = math.radians(derece)
+sinus = math.sin(radyan)
+print(round(sinus, 3))
+"""
+# endregion

566f2bba97a1a40a820d2feb7a02e6301acb5360
batukochan <batukochan@icloud.com>
2021-05-01 00:09:12 +0300

    çalışma

--- 02_karar_yapilari/0209_nested_if.py
+++ 02_karar_yapilari/0209_nested_if.py
@@ -1,7 +1,7 @@
 # region nested if örnek 1
 """
-para = True
-vakit = True
+para = False
+vakit = False
 if para == True:
     if vakit == True:
         print("Sinemaya gidebilirsiniz.")
@@ -13,7 +13,26 @@ else:
     else:
         print("Moralini yüksek tutmaya çalış, her şey güzel olacak!")
 """
+
 # endregion
+
+# region örnek 2
+"""
+vize = int(input("Lütfen vize notunuzu giriniz...\nVize:"))
+final = int(input("Lütfen final notunuzu giriniz...\nFinal:"))
+ort = .4*vize + 0.6*final
+if ort > 45 and final > 50:
+    print("Geçtiniz. Ortalamanız: ",round(ort,2))
+elif ort > 60 or final > 60:
+    print("şartlı geçiniz")
+    print("ortalamanız ",round(ort,2))
+    # vize:100-final:40
+    # vize:10-final:65
+else:
+    print("Kaldınız. Ortalamanız: ",round(ort,2))
+"""
+# endregion
+
 
 # region örnek 2
 """
@@ -40,14 +59,24 @@ else:
 # endregion
 
 # region basit örnek
-
+"""
 a = int(input("lütfen sayı giriniz... \nSayı : "))
 if a > 0:
     if a < 100:
-        print(f"{a} sayı 100 den küçüktür, pozitif")
+        print(f"{a} sayısı 100'den küçüktür, pozitif")
     else:
-        print(f"{a} sayı 100 den büyüktür, pozitif")
+        print(f"{a} sayısı 100'den büyüktür, pozitif")
 else:
     print("lütfen 0 ya da negatif değer girmeyin!")
+"""
+# endregion
+
+# region mantıksal operatörler
+"""
+print(not(2 < 3))
+# True → False
+print(not(2 == 3))
+# False → True
+"""
 
 # endregion

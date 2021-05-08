fb94674d12e6a4903ee1579730b8d5c81f173697
batukochan <batukochan@icloud.com>
2021-05-09 00:29:45 +0300

    çalışma

---
+++ 09_Turtle_Module.py/cember.py
@@ -0,0 +1,22 @@
+import math
+import turtle
+
+t = turtle.Turtle()
+
+
+def poligon(t, uzunluk, n):
+    aci = 360/n
+    for i in range(n):
+        t.fd(uzunluk)
+        t.lt(aci)
+
+
+def cember(t, r):
+    cevre = 2*math.pi*r
+    n = 50
+    uzunluk = cevre/n
+    poligon(t, n=n, uzunluk=uzunluk)
+
+
+cember(t, 100)
+turtle.mainloop()

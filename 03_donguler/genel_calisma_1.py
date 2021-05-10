fc98f3a513fd3b12eda7ca2465e3f538340ac999
batukochan <batukochan@icloud.com>
2021-05-11 01:31:12 +0300

    çalışma

---
+++ 03_donguler/genel_calisma_1.py
@@ -0,0 +1,71 @@
+# region kare çıktısı veren fonksiyon
+"""
+def yildizKareFor(n=3):
+    '''
+    n x n formatında kare yazdırır.
+    '''
+    for i in range(n):
+        for a in range(n):
+            print(" * ",end="")
+        print()
+
+yildizKareFor()
+"""
+"""
+def yildizWhile(n=3):
+    i,j = 0,0
+    while i != n:
+        i += 1
+        while j != n:
+            print(" * ",end="")
+            j += 1
+        print()
+        j = 0
+
+yildizWhile(12)
+"""
+# endregion
+
+# region üçgen
+"""
+def ucgenWhile(n=5):
+    i=1
+    while i != n:
+        print(" *"*i)
+        i += 1
+ucgenWhile(12)
+"""
+# endregion
+
+# region ters üçgen
+"""
+def tersUcgenWhile(n=5):
+    i=n
+    while i != 0:
+        print(" *"*i)
+        i -= 1
+tersUcgenWhile()
+"""
+"""
+def tersUcgenFor(n=3):
+
+    for i in range(n,0,-1):
+        print(" *"*i)
+
+tersUcgenFor(12)
+"""
+# endregion
+
+# region
+"""
+def tersUcgenFor(n=3):
+    for i in range(1,n+1,1):
+        print(" *"*i)
+        if i == n:
+            for i in range(n-1,1,-1):
+                print(" *"*i)  
+            break 
+
+tersUcgenFor(12)
+"""
+# endregion

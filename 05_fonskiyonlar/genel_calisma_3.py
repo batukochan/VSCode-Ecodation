be205a9783f8950ee98ed11bbd84a62ed69f955f
batukochan <batukochan@icloud.com>
2021-05-12 19:47:51 +0300

    çalışma

--- 05_fonskiyonlar/genel_calisma_3.py
+++ 05_fonskiyonlar/genel_calisma_3.py
@@ -1,5 +1,5 @@
 # region bool fonksiyonlar
-'''
+
 def ciftMi(x):
     """fonksiyon içerisinde verilen sayının
     tek ya da çift olma durumuna göre boolean 
@@ -7,7 +7,7 @@ def ciftMi(x):
     if x % 2 == 0:
         return True
     return False
-
+'''
 deger = ciftMi(11)
 a = ciftMi
 print(a(12)) # True → a aslında ciftMi fonksiyonu olmuştur. Yeniden adlandırma!
@@ -22,10 +22,10 @@ print(type(a)) # class <function>
 Fonksiyonumuzun alacağı parametreleri bilemediğimiz 
 durumda '*args' parametresini alır.
 '''
-"""
+
 def topla(*args):
     return sum(args)
-    
+"""    
 print(topla(2,3,4,5,6,7,8,9))
 """
 # endregion
@@ -107,11 +107,13 @@ hangiGun()
 
 # region
 
-
 def aritmatikOrt(*args):
     toplam = sum(args)
     elemanSayisi = len(args)
     ort = toplam/elemanSayisi
     return f"{elemanSayisi} adet sayının aritmatik ortalaması → {round(ort,2)}"
-
+"""
 print(aritmatikOrt(12,17,11))
+"""
+
+#endregion

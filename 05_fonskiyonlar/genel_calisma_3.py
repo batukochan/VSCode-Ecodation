5dc78ab027bde1f84a3d3ce0c328b1cd96775c81
batukochan <batukochan@icloud.com>
2021-05-12 19:47:31 +0300

    çalışma

--- 05_fonskiyonlar/genel_calisma_3.py
+++ 05_fonskiyonlar/genel_calisma_3.py
@@ -32,13 +32,86 @@ print(topla(2,3,4,5,6,7,8,9))
 
 # region
 
+
 def girdiAl(metin):
     girdi = input(metin)
-    return girdi 
+    return girdi
+
+
 """
 sayi = girdiAl("Lütfen br sayı giriniz: ")
 print(sayi)
+print(type(sayi))
 """
 # endregion
 
-#region
+# region
+'''
+olasılık 1 - girdinin başında ya da sonunda boşluk olabilir. (strip)
+olasılık 2 - sayının başına +/- yazılmış olabilir. strip(kesilecek karakterler)
+'''
+
+
+def tamSayiMi(girdi):
+    girdi = girdi.strip()
+    girdi = girdi.strip('+-')
+    if girdi.isdigit():
+        return True
+    else:
+        return False
+
+
+"""
+aciklama = "Lütfen bir tam sayı giriniz...\nSayı: "
+sayi = girdiAl(aciklama)
+print(tamSayiMi(sayi))
+print(tamSayiMi(girdiAl("Lütfen bir sayı giriniz: ")))  # tek satırda
+"""
+# endregion
+
+# region
+
+
+def tamSayiDongusu():
+    deger = tamSayiMi(girdiAl("Lütfen bir sayi giriniz: "))
+    if deger:
+        return f"Girilen veri tam sayısıdır {deger}"
+    else:
+        return tamSayiDongusu()
+
+
+"""
+print(tamSayiDongusu())
+"""
+
+# endregion
+
+# region
+
+
+def hangiGun():
+    gunler = ["", "Pazartesi", "Salı", "Çarşamba",
+              "Perşembe", "Cuma", "Cumartesi", "Pazar"]
+    gun = int(input("1 ile 7 arasında bir gün seçiniz...\Gün: "))
+    if 0 < gun <= len(gunler):
+        print(gunler[gun])
+    else:
+        print("Hatalı seçim")
+        return hangiGun()
+
+
+"""        
+hangiGun()
+"""
+# endregion
+
+# region
+
+
+def aritmatikOrt(*args):
+    toplam = sum(args)
+    elemanSayisi = len(args)
+    ort = toplam/elemanSayisi
+    return f"{elemanSayisi} adet sayının aritmatik ortalaması → {round(ort,2)}"
+
+print(aritmatikOrt(12,17,11))

1dc2b740a2f9d9c50adfdd4717824ca5702a5794
batukochan <batukochan@icloud.com>
2021-05-16 00:36:11 +0300

    çalışma

---
+++ 04_liste_string_metodlar.py/listeler_genel_calisma.py
@@ -0,0 +1,12 @@
+# region listeler
+
+"""
+    list aslında bir dizidir. List'lerde index yapısı ile içindeki 
+karakterlere ulaşılabilir.
+→ liste = []
+"""
+
+bankalar = ['halk','ziraat','ing']
+print(bankalar)
+bankalar = ['kamu',['halk','ziraat']]
+print(bankalar[1][1])

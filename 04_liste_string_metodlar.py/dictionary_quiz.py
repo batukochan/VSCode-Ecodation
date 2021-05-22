d591c0253638eaa71f97f671c3b3e33b218dc614
batukochan <batukochan@icloud.com>
2021-05-22 22:37:02 +0300

    çalışma

---
+++ 04_liste_string_metodlar.py/dictionary_quiz.py
@@ -0,0 +1,9 @@
+# region quiz
+
+file = open('C:\\Users\\batuh\\Downloads\\kelimeler.txt')
+for kelime in file:
+    kelimeler = dict()
+    kelimeler[kelime] = len(kelime)
+print(kelimeler)
+
+#devam edeceğim

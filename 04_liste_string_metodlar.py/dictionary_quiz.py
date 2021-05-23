d4d1b2fcbfd5e5b5d676bf2a1fb1eb3b9b9a0102
batukochan <batukochan@icloud.com>
2021-05-23 22:49:04 +0300

    çalışma

--- 04_liste_string_metodlar.py/dictionary_quiz.py
+++ 04_liste_string_metodlar.py/dictionary_quiz.py
@@ -1,9 +1,20 @@
 # region quiz
 
-file = open('C:\\Users\\batuh\\Downloads\\kelimeler.txt')
-for kelime in file:
-    kelimeler = dict()
-    kelimeler[kelime] = len(kelime)
-print(kelimeler)
+def kelimelerSozlugu():
+    MINKARAKTERUZUNLUGU = 19
+    # boş sözlük yaratıyoruz
+    sozluk = dict()
+    dosya = open('C:\\Users\\batuh\\Downloads\\kelime.txt')
+    for i,satir in enumerate(dosya):
+        #satır içerisindeki \n karakterleri silmeliyiz
+        satirDizisi = satir.split()
+        kelime = satirDizisi[0]
+        if len(kelime)>= MINKARAKTERUZUNLUGU:
+            #print(kelime)       
+            if not kelime in sozluk:
+                sozluk[kelime] = len(kelime)
+    return sozluk
+
+print(kelimelerSozlugu())
 
 #devam edeceğim

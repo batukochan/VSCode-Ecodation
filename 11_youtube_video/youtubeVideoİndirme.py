c2b6734636bd67434288e08759f47f0dd74329d5
batukochan <batukochan@icloud.com>
2021-06-02 01:15:46 +0300

    çalışma

--- 11_youtube_video/youtubeVideoİndirme.py
+++ 11_youtube_video/youtubeVideoİndirme.py
@@ -1,6 +1,6 @@
 from pytube import YouTube
 
-url = 'https://www.youtube.com/watch?v=AMpkXMU0IkI'
+url = 'https://www.youtube.com/watch?v=ZFsHrySUAwc'
 
 video = YouTube(url)
 """
@@ -9,5 +9,3 @@ print(video.thumbnail_url) # küçük resim
 print(video.streams.get_highest_resolution())
 """
 video.streams.get_highest_resolution().download()
-
-path = 'C:\Users\batuh\Desktop\VSCode-Python Dersleri\JavaScript ile Güncel Hava Durumu Uygulaması - OpenWeather API Kullanımı.mp4'

2802cfca6c0f8b3ece34b92d40d9c515d4e06fd0
batukochan <batukochan@icloud.com>
2021-06-04 16:36:48 +0300

    çalışma

---
+++ 07_moduller_paketler/0702_kendi_modulunu_yazmak.py
@@ -0,0 +1,37 @@
+# region modul import etme
+
+"""
+import ilkModul
+"""
+
+# modul bir fonksiyon olsaydı
+
+"""
+import ilkModul
+
+ilkModul.selamla()
+"""
+
+# endregion
+
+# region girdi işlemleri
+
+import girdiİslemleri
+
+# girdiİslemleri.girdiİste()
+
+# metin iste ve ekrana yaz
+
+"""
+metin = girdiİslemleri.stringAl()
+print(metin)
+"""
+"""
+a = girdiİslemleri.tamSayiAl()
+print(a)
+"""
+
+say = girdiİslemleri.ondalikSayiİste()
+print(say)
+
+# endregion
---
+++ 07_moduller_paketler/ilkModul.py
@@ -0,0 +1,6 @@
+"""
+print("İlk Modül")
+"""
+
+def selamla():
+    print("merhaba")

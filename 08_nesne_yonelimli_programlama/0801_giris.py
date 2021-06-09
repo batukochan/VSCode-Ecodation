37407076275ea74c121f0234b2cb86bd6bc84533
batukochan <batukochan@icloud.com>
2021-06-09 23:38:14 +0300

    çalışma

--- 08_nesne_yonelimli_programlama/0801_giris.py
+++ 08_nesne_yonelimli_programlama/0801_giris.py
@@ -22,5 +22,22 @@ class Insan:
 i1 = Insan() 
 i1.ad = "ali"
 i1.soyad = "koç"
-print(i1.ad,i1.soyad)
+# print(i1.ad,i1.soyad)
+
+
+# örnek
+
+class Ogrenci:
+    adSoyad = ""
+    not1 = 0
+    not2 = 0
+
+o1 = Ogrenci()
+o1.adSoyad = input("Öğrenci adı ve soyadı: ")
+o1.not1 =int(input("Öğrenci not1: "))
+o1.not2 =int(input("Öğrenci not2: "))
+
+ortalama = (o1.not1 + o1.not2) / 2
+print(ortalama)
+
 

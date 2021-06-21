9b080f6e61c028a3099f41e09dd4e4107d70a2d9
batukochan <batukochan@icloud.com>
2021-06-21 21:10:06 +0300

    çalışma

---
+++ 08_nesne_yonelimli_programlama/0807_class_metodu.py
@@ -0,0 +1,30 @@
+# region class metod
+
+class Insan:
+    insanlar = []
+
+    def __init__(self, ad, soyad) -> None:
+
+        self.ad = ad
+        self.soyad = soyad
+        Insan.Ekle(self)
+
+    @classmethod  # dekorator
+    def Ekle(cls, insanNesnesi):
+        Insan.insanlar.append(insanNesnesi)
+
+    def __str__(self) -> str:
+        return f"{self.ad} {self.soyad}"
+
+    @classmethod
+    def Parcala(cls, string):
+        liste = string.split(",")
+        return cls(liste[0], liste[1])
+
+
+i1 = Insan("ali", "veli")
+
+print(i1)
+
+for i in Insan.insanlar:
+    print(i)

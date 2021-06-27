99845202f18b5af25e8e6ad7ff6c769b972d35cf
batukochan <batukochan@icloud.com>
2021-06-27 16:04:36 +0300

    çalışma

--- 08_nesne_yonelimli_programlama/0808_class_method_egzersiz.py
+++ 08_nesne_yonelimli_programlama/0808_class_method_egzersiz.py
@@ -1,7 +1,43 @@
 
 
 class User:
+    userListe = []
 
-    def __init__(self,kullaciNick,kullaniciPass) -> None:
-        pass
-    
+    def __init__(self, kullaciNick, kullaniciPass) -> None:
+        self.kullaniciNick = kullaciNick
+        self.kullaniciPass = kullaniciPass
+        User.ekle(self)
+
+    @classmethod
+    def ekle(cls, us):
+        cls.userListe.append(us)
+
+    @classmethod
+    def olustur(cls, string=str()):
+        liste = string.split(",")
+        return cls(liste[0], liste[1])
+
+class Sistem:
+    def __init__(self,ad) -> None:
+        self.ad = ad
+
+    def Login(self,kulAd,kulSifre):
+        isLogin = False
+        for item in User.userListe:
+            if item.kullaniciNick == kulAd and item.kullaniciPass == kulSifre:
+                isLogin = True
+        if isLogin:
+            print(f"{self.ad} Sistemine hoşgeldiniz {kulAd}")
+        else:
+            print("Hatalı giriş")
+
+# u1 = User('admin', '123')
+# u2 = User('admin', '123456')
+User.olustur("admin,123")
+User.olustur("admin,123456")
+s1=Sistem("windows")
+ad=input("Ad: ")
+pw=input("Şifre: ")
+s1.Login(ad,pw)
+for item in User.userListe:
+    print(item.kullaniciNick, item.kullaniciPass)
